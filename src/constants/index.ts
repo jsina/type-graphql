f88adfe5283dec474ae09ca70e65df7096fed983
jsina <jsina.maleki@gmail.com>
2020-11-26 23:19:26 +0330

    add constants 💬

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,6 @@
 export const SESSION_NAME = "uid";
 export const SESSION_SECRET = process.env.SESSION_SECRET || "123";
+export const SMTP_HOST = process.env.SMTP_HOST || "";
+export const SMTP_PORT = parseInt(process.env.SMTP_PORT!);
+export const MAIL_USERNAME = process.env.MAIL_USERNAME;
+export const MAIL_PASSWORD = process.env.MAIL_PASSWORD;
